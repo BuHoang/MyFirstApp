b42abeb6920f4da831f2ffa2ac60f53f9e49a6ea
Bui Hoang <hoangbvgch17056@fpt.edu.vn>
2019-05-07 15:16:41 +0700

    Update handling.php

--- handling.php
+++ handling.php
@@ -2,11 +2,12 @@
    require_once('./dbconnector.php');
    if (isset($_POST['them'])) {
         //them
+    $id=$_POST['id'];
     $name=$_POST['name'];
     $job=$_POST['job'];
     $address=$_POST['address'];
     $salary=$_POST['salary'];
-    $sql="Insert Into staff1 (name, job, address, salary) values('".$name."','".$job."','".$address."','".$salary."')";
+    $sql="Insert Into staff1 (name, job, address, salary) values(".$ID.",'".$name."','".$job."','".$address."','".$salary."')";
     $cn = new DBConnector();
     $return = $cn->execStatement($sql);
     header('location:index.php');
@@ -15,7 +16,8 @@
     $id=$_GET['id'];
     $sql="DELETE FROM staff1 WHERE id = $id";
     $cn = new DBConnector();
-    $return = $cn->execStatement($sql);    
+    $return = $cn->execStatement($sql);
+    echo($return);    
     header('location:index.php'); 
     }
 ?>
